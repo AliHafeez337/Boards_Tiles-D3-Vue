96d55ac7e5a0e16773c964936eea031aa073f839
AliHafeez337 <alihafeez337@gmail.com>
2020-07-04 13:59:13 +0500

    38th - Did, trailer and truck arrangement, only trucks arrangement and very much improvements....

--- src/CONFIG.js
+++ src/CONFIG.js
@@ -133,4 +133,12 @@ export const config = {
   text_size: '15',
   text_color: 'Black',
   mouse_leave_opacity: 0,
+
+  // Arrange
+  y_gap_between_tiles: 10,
+  x_gap_between_tiles: 10,
+  y_appart_from_section: 30,
+  x_appart_from_section: 10,
+  no_of_tiles_in_a_row: 3,
+
 }
