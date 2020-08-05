9a3e6d603f73069194e400abbb2461d55c0651c8
AliHafeez337 <alihafeez337@gmail.com>
2020-08-05 12:56:52 +0500

    80th - Socket.io setup...

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -262,6 +262,7 @@ router.get(
   passport.authenticate('jwt', {session: false}),
   ensureAuthenticated, 
   async (req, res, next) => {
+    // console.log(req.connections.handshake.headers.connection)
     // console.log(Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 1) + new Date().getTime())
     res.send(req.user);
   }
